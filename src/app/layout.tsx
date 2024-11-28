e2b505e1beab59cfa0c6b10f0213afb9442c558f
Elostay <nastia60203@gmail.com>
2024-11-28 15:52:58 +0200

    blackout save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import { spaceGroteskRegular } from "@/fonts/fonts";
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "Anastasiia Motsukh portfolio",
@@ -16,11 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={spaceGroteskRegular.className}>
-        <div className="">
-          <Header />
-          <main>{children}</main>
-        </div>
+      <body className={clsx("overflow-hidden", spaceGroteskRegular.className)}>
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
       <div className="container mx-auto px-4 ">
         <div
           className={clsx(
-            "p-8  backdrop-blur-md absolute top-1/3 left-1/4 rounded-3xl dark:bg-white dark:bg-opacity-80  ",
+            "p-8 backdrop-blur-md absolute top-1/3 md:left-1/4 rounded-3xl dark:bg-white dark:bg-opacity-80  w-[300px] m-auto",
             CardoRegular.className
           )}
         >
-          <p className="border-b border-black pb-2 mb-2 text-8xl ">
+          <p className="border-b border-black pb-2 mb-2 text-3xl md:text-8xl font-semibold whitespace-nowrap">
             Anastasiia Motsukh
           </p>
-          <p className="text-4xl">Frontend Developer</p>
+          <p className="text-xl md:text-4xl">Frontend Developer</p>
         </div>
       </div>
     </section>
